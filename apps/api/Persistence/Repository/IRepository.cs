bfe5405477b286ef9a60152bb94c655d1146b1f4
Pilipchuk Bogdan <alenwhalker@gmail.com>
2022-08-26 12:18:01 +0300

    move Pageable to persistance layer

--- apps/api/Persistence/Repository/IRepository.cs
+++ apps/api/Persistence/Repository/IRepository.cs
@@ -2,13 +2,18 @@ using System.Linq.Expressions;
 
 namespace Persistence.Repository;
 
-public interface IGenericRepository<TEntity>
+public interface IRepository<TEntity>
 {
     Task<TEntity> Save(TEntity entity);
     Task<TEntity> FindOne(int id);
     Task<ICollection<TEntity>> FindMany(
         Expression<Func<TEntity, bool>> filter,
         Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy
+    );
+    Task<ICollection<TEntity>> FindMany(
+        Expression<Func<TEntity, bool>> filter,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy,
+        IPageable pageable
     );
     Task DeleteOne(int id);
 }
--- apps/api/Persistence/Repository/Pageable.cs
+++ apps/api/Persistence/Repository/Pageable.cs
@@ -1,4 +1,4 @@
-namespace Application.Services;
+namespace Persistence.Repository;
 
 public class Pageable : IPageable
 {
