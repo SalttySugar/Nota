e62765aa8385e36f09c3b9c77caa64d934889a8f
Pilipchuk Bogdan <alenwhalker@gmail.com>
2022-08-28 07:36:52 +0300

    add missing methods to repository

--- apps/api/Persistence/Repository/IRepository.cs
+++ apps/api/Persistence/Repository/IRepository.cs
@@ -5,15 +5,18 @@ namespace Persistence.Repository;
 public interface IRepository<TEntity>
 {
     Task<TEntity> Save(TEntity entity);
-    Task<TEntity> FindOne(int id);
+    Task<TEntity?> FindOne(int id);
     Task<ICollection<TEntity>> FindMany(
-        Expression<Func<TEntity, bool>> filter,
-        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy
+        Expression<Func<TEntity, bool>>? filter,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy
     );
     Task<ICollection<TEntity>> FindMany(
-        Expression<Func<TEntity, bool>> filter,
-        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy,
+        Expression<Func<TEntity, bool>>? filter,
+        Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy,
         IPageable pageable
     );
+
+    Task<int> Count();
+    Task<int> Count(Expression<Func<TEntity, bool>> filter);
     Task DeleteOne(int id);
 }
