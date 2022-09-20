59bd7835f1c0865d6e9a68039440cacdf4f472ab
Pilipchuk Bogdan <alenwhalker@gmail.com>
2022-09-20 10:56:50 +0300

    add storybook configuration

---
+++ apps/frontend-e2e/src/e2e/nx-welcome.component.cy.ts
@@ -0,0 +1,6 @@
+describe('frontend', () => {
+  beforeEach(() => cy.visit('/iframe.html?id=nxwelcomecomponent--primary'));
+  it('should render the component', () => {
+    cy.get('nota-nx-welcome').should('exist');
+  });
+});
